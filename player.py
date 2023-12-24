2306c4575b8a4e9d336351957adfe9664dcc229e
calamoazul <info@calamoazul.com>
2023-12-24 19:25:21 +0100

    Mostrando mensaje reproductor vacío

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ from downloader import Downloader
 import os.path
 import os
 from tkinter import Frame, Label, Button
+from helpers import *
 #Clase para el reproductor de música
 
 
@@ -15,21 +16,18 @@ class Player(Downloader):
         self.songs = []
         self.play_song = None
     def get_player(self, app):
-        canciones = os.listdir(self.dir)
         with os.scandir(self.dir) as songs:
             for song in songs:
                 if(song not in self.songs):
                     self.songs.append(song.name)
         player = Frame(app, background="#313131", padx=50, pady=50, height=100)
-        self.play_song = self.songs[0]
-        play_song = Label(player, text=self.play_song, background="#313131", foreground="#22C55E")
-        play_song.pack()
-        try:
-            print(self.dir)
+        if(len(self.songs) >= 1 ):
+            self.play_song = self.songs[0]
             pygame.mixer.init()
             pygame.mixer.music.load(os.path.join(self.dir, self.songs[0]))
             pygame.mixer.music.play()
-        except Exception as error:
-            print(canciones[0])
-            print(error)
+        else:
+            self.play_song = 'No hay canciones en el reproductor'
+        play_song = Label(player, text=self.play_song, background="#313131", foreground="#22C55E")
+        play_song.pack()
         return player
